30300f23b01f2321680a5efb1205e274771d4aa5
Uwe Krohn <krohn@fh-luebeck.de>
2021-02-07 15:38:31 +0100

    FunctionsHaskellTemplate hinzugefuegt

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for FunctionsHTemplate
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Operators
+import Functions
+import Composition
+
+main :: IO ()
+main = return ()
