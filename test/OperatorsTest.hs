cff0e6ae941d091384a9a2784ed7e8f306561333
Uwe Krohn <krohn@fh-luebeck.de>
2021-02-14 15:53:14 +0100

    test

--- test/OperatorsTest.hs
+++ test/OperatorsTest.hs
@@ -10,7 +10,7 @@ prop_commutative x y = add x y == add y x
 
 prop_commutativeRec x y = x>0&&y>0 ==> addRec x y == addRec y x
 
-prop_max x y = Operators.max x y == Prelude.max x y
+-- prop_max x y = Operators.max x y == Prelude.max x y
 
 
 
