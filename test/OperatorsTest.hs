94059f5f3f3de4ff65f0a0d0f469a7b2fc7b8824
Uwe Krohn <krohn@fh-luebeck.de>
2021-02-15 08:43:39 +0100

    exitfailure

--- test/OperatorsTest.hs
+++ test/OperatorsTest.hs
@@ -7,7 +7,7 @@ import System.Exit(exitSuccess, exitFailure)
 
 import Operators
 
-prop_commutative x y = add x y == add y x
+prop_commutative x y = add x y == add y 1
 
 prop_commutativeRec x y = x>0&&y>0 ==> addRec x y == addRec y x
 
