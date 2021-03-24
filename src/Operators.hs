0754b0cbeff38c79b625fc32a782d37a9730f941
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-03-24 16:11:49 +0100

    done_1

--- src/Operators.hs
+++ src/Operators.hs
@@ -12,4 +12,29 @@ succ :: Int -> Int
 succ x = x + 1
 
 max :: Int -> Int -> Int
-max a b = 0
+max a b
+  | a > b = a
+  | True = b
+
+not :: Bool -> Bool
+not a
+  | a = False
+  | True = True
+
+or' :: Bool -> Bool -> Bool
+or' a b = a || b
+
+and' :: Bool -> Bool -> Bool
+and' a b = a && b
+
+
+nor' :: Bool -> Bool -> Bool
+nor' a b = not (or' a b)
+
+
+nand' :: Bool -> Bool -> Bool
+nand' a b = not (and' a b)
+
+
+odd' :: Int -> Bool
+odd' a = mod a 2 == 1
