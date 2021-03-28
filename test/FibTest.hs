9ada884eaf181e70618590f52a49e201e8b4a4d6
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-03-28 21:10:46 +0200

    done_3

--- test/FibTest.hs
+++ test/FibTest.hs
@@ -7,7 +7,29 @@ import Test.QuickCheck
 import System.Exit(exitSuccess, exitFailure)
 
 
-prop_fib = False
+prop_fib_test_1 = do
+     n <- choose (0, 30) :: Gen Int
+     m <- choose (1, 30) :: Gen Int
+     return $ fib (m+n) == fib (n+1) * fib m + fib n * fib(m-1)
+                       
+                       
+                       
+prop_fib_test_2 = do
+     n <- choose (1, 30) :: Gen Int
+     m <- choose (1, 30) :: Gen Int
+     return $ ggT (fib m) (fib n) == fib (ggT m n) 
+
+
+
+prop_fib_test_3 = do
+    n <- choose (1, 30) :: Gen Int
+    return $ ggT (fib n) (fib n+1) == 1 
+
+
+
+
+
+
 
 return []
 runTests = $quickCheckAll
--- test/GgtTest.hs
+++ test/GgtTest.hs
@@ -12,8 +12,23 @@ prop_idempotence_test a = a>0 ==> ggT a a == a
 prop_neutrales_element a = a > 0 ==> ggT a 0 == a
 prop_absorbierendes_element a = a > 0 ==> ggT a 1 == a
 prop_kommutattivgesetz a b = a > 0 && b > 0 ==> ggT a b == ggT b a
--- prop_assoziativgesetz a b c = a > 0 && b > 0 && c > 0 ==> ggT(a ggT(b c)) == ggT (ggT a b c)
+prop_assoziativgesetz = do
+     a <- choose (1, 30) :: Gen Int
+     b <- choose (1, 30) :: Gen Int
+     c <- choose (1, 30) :: Gen Int
+     return $ ggT a (ggT b c) == ggT (ggT a b) c
 
+prop_disitributibgesetz = do
+    a <- choose (1, 30) :: Gen Int
+    b <- choose (1, 30) :: Gen Int
+    m <- choose (1, 30) :: Gen Int 
+    return $ ggT (m*a) (m*b) == m * ggT a b
+    
+    
+prop_last_test_1 a b = a > 0 && b > 0 && a > b ==> ggT a b == ggT (a-b) b
+ 
+prop_last_test_2 a b = a > 0 && b > 0 && a < b ==> ggT a b == ggT a (b-a)
+  
 
 
 return []
